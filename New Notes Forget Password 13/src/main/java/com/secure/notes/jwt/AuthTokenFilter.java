d59b4b72263672fc161731d2d51d1e94b0dcb024
ShubhS6 <shubhamvishwakarma732@gmail.com>
2026-05-12 20:59:51 +0530

    reset pass completed

---
+++ New Notes Forget Password 13/src/main/java/com/secure/notes/jwt/AuthTokenFilter.java
@@ -0,0 +1,52 @@
+package com.secure.notes.jwt;
+
+import com.secure.notes.securityService.UserDetailsServiceImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class AuthTokenFilter extends OncePerRequestFilter {
+    private static final Logger logger= LoggerFactory.getLogger(AuthTokenFilter.class);
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try{
+            String jwt= parsejwt(request);
+            if(jwt !=null && jwtUtils.validateJwtToken(jwt)){
+                String username=jwtUtils.getUserNameFromJwtToken(jwt);
+                UserDetails userDetails=userDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken authentication=new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }catch (Exception e){
+            logger.error("cant set user authentication ",e);
+        }
+        filterChain.doFilter(request,response);
+    }
+
+
+    private String parsejwt(HttpServletRequest request){
+        String jwt=jwtUtils.getJwtFromHeader(request);
+        return jwt;
+    }
+}
---
+++ New Notes Forget Password 13/src/main/java/com/secure/notes/service/UserService.java
@@ -0,0 +1,30 @@
+package com.secure.notes.service;
+
+import com.secure.notes.dto.UserDto;
+import com.secure.notes.model.Role;
+import com.secure.notes.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void updateUserRole(Long userId,String roleName);
+    List<User> getAllUser();
+    UserDto getUserById(Long id);
+    User findByUsername(String username);
+
+    void updateAccountLockStatus(Long userId, boolean lock);
+
+    List<Role> getAllRoles();
+
+    void updateAccountExpiryStatus(Long userId, boolean expire);
+
+    void updateAccountEnabledStatus(Long userId, boolean enabled);
+
+    void updateCredentialsExpiryStatus(Long userId, boolean expire);
+
+    void updatePassword(Long userId, String password);
+
+    void generatePasswordResetToken(String email);
+
+    void resetPassword(String token, String newPassword);
+}
