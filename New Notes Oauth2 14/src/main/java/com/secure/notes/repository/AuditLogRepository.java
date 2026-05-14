81e58f2d2a2163c26598295dc1af20308021a6f9
ShubhS6 <shubhamvishwakarma732@gmail.com>
2026-05-14 22:09:51 +0530

    oauth2

---
+++ New Notes Oauth2 14/src/main/java/com/secure/notes/repository/AuditLogRepository.java
@@ -0,0 +1,12 @@
+package com.secure.notes.repository;
+
+import com.secure.notes.model.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AuditLogRepository extends JpaRepository<AuditLog,Long> {
+    List<AuditLog> findByNoteId(Long id);
+}
